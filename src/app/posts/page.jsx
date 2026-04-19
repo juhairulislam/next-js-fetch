26700ea811f82611032019e309fd64720acc5715
juhairulislam <juhairulislam2018@gmail.com>
2026-04-19 17:51:21 +0600

    set JSON server with temporary data server

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Posts = () => {
+const Posts =async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts') ;
+    const posts = await res.json()
+
     return (
         <div>
-            <h1>This is posts section</h1>
+            <h1>Post are coming soon :{posts.length}</h1>
         </div>
     );
 };
